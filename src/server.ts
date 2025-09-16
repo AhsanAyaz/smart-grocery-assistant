5e9df0df75cfe050369be1901c0653654dcda57a
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2025-09-17 00:59:52 +0200

    feat(build): add deploy script for pm2

--- src/server.ts
+++ src/server.ts
@@ -88,7 +88,7 @@ app.use((req, res, next) => {
 /**
  * Start the server if this module is the main entry point.
  */
-if (isMainModule(import.meta.url)) {
+if (isMainModule(import.meta.url) || process.env['PM2'] === 'true') {
   const port = process.env['PORT'] || 4000;
   app.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
